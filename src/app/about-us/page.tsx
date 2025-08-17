70ab9f558098e4ec5925b06366f987db2b532820
midlaj <mumidlaj@gmail.com>
2025-08-17 06:23:50 +0530

    removed (RISE) from core values heading

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -335,7 +335,7 @@ export default function AboutPage() {
           </motion.div>
 
           <div>
-            <SectionHeader>Our Core Values (RISE)</SectionHeader>
+            <SectionHeader>Our Core Values</SectionHeader>
             <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 max-w-6xl mx-auto">
               {coreValues.map((value, index) => (
                 <RiseCard key={value.letter} {...value} delay={0.2 * index} />
