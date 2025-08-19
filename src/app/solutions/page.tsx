1ed24ad61d62e9ae891a5207b48e20dadce3e981
midlaj <mumidlaj@gmail.com>
2025-08-19 10:59:39 +0530

    solutions overview page updates

--- src/app/solutions/page.tsx
+++ src/app/solutions/page.tsx
@@ -1,41 +1,11 @@
-/* eslint-disable @next/next/no-img-element */
-"use client";
-import HeroBg from "@/components/hero-bg";
-import { SolutionsOverview } from "../components/solution-overview";
-import { motion } from "framer-motion";
+import { StepScrollContainer } from "@/components/custom/step-scroll";
+import { SolutionsCards } from "./solutions-card";
+import SolutionsHero from "./solutions-hero";
 
 export default function SolutionsPage() {
-  return (
-    <div className="min-h-screen ">
-      <HeroBg image="/solution.png" video="/solution.mp4">
-        <section className="relative h-screen flex items-center justify-center overflow-hidden">
-          <div className="relative z-10 text-center text-white px-4">
-            <motion.h1
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.8, ease: "easeOut" }}
-              className="text-5xl md:text-8xl font-bold mb-6 bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-300"
-            >
-              Solutions
-            </motion.h1>
-            <motion.p
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.8, delay: 0.2, ease: "easeOut" }}
-              className="text-xl md:text-2xl text-gray-200 max-w-2xl mx-auto"
-            >
-              At Klimalogia, we deliver end-to-end climate and sustainability
-              solutions designed to help your business meet its goals.
-            </motion.p>
-          </div>
-        </section>
-      </HeroBg>
-
-      <div id="nav-trigger" />
-
-      <div className="min-h-screen w-full relative bg-white">
-        <SolutionsOverview page />
-      </div>
-    </div>
-  );
+  const sections = [
+    { id: "hero", component: <SolutionsHero />, name: "Hero" },
+    { id: "solutions", component: <SolutionsCards />, name: "Solutions" },
+  ];
+  return <StepScrollContainer sections={sections}></StepScrollContainer>;
 }
